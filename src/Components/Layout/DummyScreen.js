72a994aff27409cf2691be9ac53251b952917ef4
AdityaPratap24 <adityapratap2401@gmail.com>
2023-05-12 14:42:32 +0530

    third commit

--- src/Components/Layout/DummyScreen.js
+++ src/Components/Layout/DummyScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
 
 const DummyScreen = () => {
   return (
@@ -6,6 +7,7 @@ const DummyScreen = () => {
       <h1 className="fst-italic" >
         Welcome to expanse tracker!!!
       </h1>
+      <span className='fst-italic bg-warning'>Your profie is incomplete<Link className='text-primary' to="/details">Complete now</Link></span>
       <hr/>
     </div>
   )
